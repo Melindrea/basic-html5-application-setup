0a2391428a386e87cb3b657c85f23be8c492693a
Marie Hogebrandt <webmistress@melindrea.net>
2012-06-17 01:07:20 +0200

    Fully working modules, and spanking new node_modules under src

--- application/src/grunt.js
+++ application/src/grunt.js
@@ -118,31 +118,6 @@ module.exports = function(grunt) {
     
     grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-contrib');
-    
-/*
-* grunt-smushit
-* https://github.com/heldr/grunt-smushit
-*
-* Copyright (c) 2012 Helder Santana
-* heldr.com
-* MIT License
-*/
-
-    /* based on grunt-recess - Thanks Sindre Sorhus*/
-    /*grunt.registerMultiTask('smushit', 'remove unnecessary bytes from image files', function() {
-        var smushit = require('node-smushit'),
-            files = this.file.src || this.file.dist || this.file;
-
-        if (typeof files !== 'string') {
-            files = grunt.file.expandFiles(files);
-        }
-
-        if (files.length) {
-            smushit.smushit(files);
-        } else {
-            grunt.fail.fatal('Image not found, please check if you put the right path.');
-        }
-    });*/
-
+    //grunt.loadNpmTasks('grunt-smushit');
 };
 
